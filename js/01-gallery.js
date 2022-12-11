c94bf62ea1e820b0ff5aad351233d920a0fed5bb
VitaBabenko <babenkovita1988@gmail.com>
2022-12-11 23:37:15 +0200

    done 1, 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+// console.log(gallery);
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`}).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+// console.dir(gallery);
+
+gallery.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+    evt.preventDefault();
+    if (evt.target.classList.contains('gallery__image')) {
+        const source = evt.target.dataset.source;
+        // console.log(source);
+
+        const instance = basicLightbox.create(`
+        <img src="${source}"/>
+        `)
+        instance.show();
+
+        document.addEventListener('keydown', onEscKeyDown);
+
+        function onEscKeyDown(evt) {
+            // console.log(evt.code);
+            if (evt.code === 'Escape') {
+                instance.close();
+                document.removeEventListener('keydown', onEscKeyDown);
+            } 
+        }
+    }
+}
+
+
+
+
+
+
